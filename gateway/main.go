b7f83431e64fcd22eff63fc7020270a62d92fbb5
Asendar1 <hamzahtaimour@gmail.com>
2026-02-02 16:47:31 +0300

    changed folder name and structure

--- gateway/main.go
+++ gateway/main.go
@@ -5,10 +5,15 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
 	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.RequestID)
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "static/index.html")
