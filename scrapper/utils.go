1a7a545ed10bb6a57edac56ebd9c2ff7dec2c8fe
Asendar1 <hamzahtaimour@gmail.com>
2026-02-05 19:09:15 +0300

    Some TODOs. TODO: add proper shutdown

--- scrapper/utils.go
+++ scrapper/utils.go
@@ -9,25 +9,7 @@ import (
 	"github.com/gocolly/colly/v2"
 )
 
-// func ValidateURL(url string) bool {
-// 	req, err := http.NewRequest("GET", url, nil)
-// 	if err != nil || req.URL.Scheme == "" || req.URL.Host == "" {
-// 		return false
-// 	}
-
-// 	req.Header.Set("User-Agent", "Mozilla/5.0 (compatible; ValidateURL/1.0)")
-
-// 	resp, err := http.DefaultClient.Do(req)
-// 	if err != nil {
-// 		return false
-// 	}
-// 	defer resp.Body.Close()
-// 	if resp.StatusCode < 200 || resp.StatusCode >= 400 {
-// 		return false
-// 	}
-// 	return true
-// }
-
+// TODO: half the file content is empty lines, make files cleaner
 func Scrape(url string) bool {
 	fileHash := sha256.Sum256([]byte(url))
 	fileName := "data/" + hex.EncodeToString(fileHash[:]) + ".txt"
